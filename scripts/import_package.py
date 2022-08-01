721bcada0705cc61669c4e884af838de99d66013
Mani Babu <bitsian.mani@gmail.com>
2022-08-01 19:55:57 +0200

    Implement the data cleanup in OOPs

--- scripts/import_package.py
+++ scripts/import_package.py
@@ -1,11 +1,14 @@
-import bank_deposit_classifier.sample as s
-import pandas as pd
-# s.print_hi()
-def data():
-    data =pd.DataFrame({ 'y': [0,0,0,0,0,0,0,1,1,1],
-    'x1':[1,2,1,2,1,2,1,5,6,5]
-    })
+from bank_deposit_classifier.data_prep import *
 
-    return data
-x =data()
-s.upsample_minority_class(x,'y',0.5)    
+
+input_path = 'input/bank-additional-full.csv'
+train_path = 'intermediate/train.csv'
+test_path = 'intermediate/test.csv'
+config_path= 'data_prep.yaml'
+
+dp = DataPrep.from_yaml(config_path)
+data = dp.get_data(input_path)
+data, encoder = dp.encode_data(data)
+train, test = dp.split_data(data)
+dp.save_data(train, train_path)
+dp.save_data(test, test_path)
